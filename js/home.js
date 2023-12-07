9468739c168e879dcd3df71dc9dfdec186ae15a3
Lawrence <lawrence@dotdot.cc>
2023-12-06 21:45:38 -0500

    Update Portfolio

---
+++ js/home.js
@@ -0,0 +1,111 @@
+// Portfolios [Name, Logo, Date, iOS, Android, Mobile, Tablet, Desktop, Client, Link, Description]
+var quickclick =        [ 'QuickClick', true, '2023', false, false, true, true, true, 'Taiwan', 'DotDot', 'Website', 'https://www.quickclick.cc',
+                        "QuickClick, Taiwan's premier online food ordering platform, where seamlessly designed system ensures an effortlessly enjoyable and visually intuitive dining experience.<ol><li>Visual Menu Clarity:<br>Clearly presented menus for easy and quick choices.</li><li>Efficient Order Customization:<br>Easily tailor your orders to your preferences.</li><li>Simplified Transaction Process:<br>Complete transactions with simplicity and clarity.</li></ol>" ];
+var co =                [ 'CO', false, '2022', false, true, true, true, true, 'Taiwan', 'Classified', '', '',
+                        "CO, an advanced tablet-based online food ordering system designed for restaurants, where our seamlessly crafted interface ensures an intuitive and efficient operational experience.<ol><li>Intuitive Order Management:<br>Streamline restaurant operations with an easy-to-use order management system.</li><li>Visual Menu Editing:<br>Efficiently edit and update menus with a visually intuitive interface.</li><li>Customizable Table Assignments:<br>Easily assign and manage orders according to specific table numbers.</li></ol>" ];
+var ecommerce =         [ 'E-commerce', false, '2023', true, true, false, false, false, 'Taiwan', 'Classified', '', '',
+                        "E-commerce, the supermarket's cutting-edge mobile app, where a thoughtfully designed interface ensures a seamless and enjoyable shopping experience for users.<ol><li>Visual Product Presentation:<br>View products with clear images and organized displays for enhanced visual appeal</li><li>Personalized Recommendations:<br>Receive tailored product suggestions based on past purchases and preferences.</li><li>Real-Time Order Tracking:<br>Monitor the status and location of your order in real-time for added convenience.</li></ol>" ];
+var foodie =            [ 'Foodie', false, '2021', true, true, false, false, false, 'Hong Kong', 'Personal Project', 'Figma', 'https://www.figma.com/file/I2ht2hmee5PsrcUeo18DGB/Zeroly?type=design&node-id=0%3A1&mode=design&t=0j9LrzZwdAslZWWv-1',
+                        "Foodie, the local restaurant discount listing app that seamlessly integrates ordering and payment functionalities, offering users a visually intuitive and efficient platform for discovering and enjoying exclusive dining deals.<ol><li>Simplified Deal Discovery:<br>Easily browse and discover local restaurant discounts with a user-friendly interface.</li><li>Secure Payment Options:<br>Enjoy secure and versatile payment methods for a worry-free transaction.</li><li>Interactive Restaurant Ratings:<br>Access and contribute to interactive restaurant ratings and reviews for informed decision-making.</li></ol>" ];
+var trainTicketing =    [ 'Train Ticketing', false, '2020', true, true, false, false, false, 'Singapore', 'Personal Project', 'Figma', 'https://www.figma.com/file/ZcwhD5zijGIQffI9OjcGAk/KTMB-MobTicket?type=design&node-id=0%3A1&mode=design&t=xan53bLLNewSQSvI-1',
+                        "Train Ticketing, the go-to app for hassle-free train reservations, where a thoughtfully designed interface ensures an effortless and enjoyable booking experience.<ol><li>Visual Seat Selection:<br>Select your preferred seats with clear visuals for an enhanced booking experience.</li><li>Real-Time Schedule Updates:<br>Stay informed with instant notifications and updates on train schedules and delays.</li><li>E-Ticket and Boarding Pass:<br>Access and manage e-tickets and boarding passes directly through the app for added convenience.</li></ol>" ];
+var advertisement =     [ 'Advertisement', true, '2021', true, true, false, false, false, 'Hong Kong', 'Classified', '', '',
+                        "Advertisement, the go-to app for managing physical advertising boards, where an intuitively designed interface ensures a seamless and efficient platform for discovering and organizing diverse advertising spaces.<ol><li>Visual Board Listings:<br>View detailed listings with clear images and organized information for enhanced visual clarity.</li><li>Booking and Reservation Functionality:<br>Book and reserve advertising spaces directly through the app for added convenience.</li><li>Interactive Feedback System:<br>Provide and receive feedback on advertising board effectiveness, enhancing the decision-making process.</li></ol>" ];
+var taoyuanPoint =      [ 'Taoyuan Point', true, '2022', false, false, true, true, true,'Taiwan', 'Classified', 'Web App', 'https://portal.typoint.tw/guest?redirectPath=home',
+                        "Taoyuan Point, the premier destination for coupons and interactive mini-games, where a thoughtfully designed interface ensures a delightful and user-friendly experience.<ol><li>Effortless Coupon Exploration:<br>Easily browse and discover a variety of coupons with a streamlined and intuitive interface.</li><li>Engaging Mini-Games:<br>Enjoy interactive and entertaining mini-games for added fun and rewards.</li><li>User-Friendly Redemption Process:<br>Redeem coupons and rewards seamlessly through a straightforward and convenient process.</li></ol>" ];
+var ttpush =            [ 'TTPush', true, '2022', true, true, true, true, true,'Taiwan', 'Classified', 'Website', 'https://www.ttpush.app',
+                        "TTPush, the go-to web app for earning free points and planning your travels, where an intuitively crafted interface ensures a smooth and enjoyable experience.<ol><li>Effortless Points Accumulation:<br>Easily earn free points through various activities with a user-friendly interface.</li><li>Real-Time Points Tracking:<br>Stay updated with instant notifications on points earned and available redemption options.</li><li>Streamlined Redemption Process:<br>Redeem points for travel rewards through a straightforward and convenient process.</li></ol>" ];
+var countdown =         [ 'Countdown', false, '2020', false, false, true, true, true,'Malaysia', 'Classified', '', '',
+                        "Countdown, the premier website for business countdowns, where a carefully crafted interface ensures a straightforward and visually appealing experience.<ol><li>Clear Visual Representation:<br>Showcase countdowns through visually appealing designs for enhanced clarity.</li><li>Real-Time Countdown Updates:<br>Stay informed with live updates on countdowns and upcoming business events.</li><li>Interactive Countdown Sharing:<br>Share countdowns effortlessly with colleagues and clients through interactive links.</li></ol>" ];
+var company =           [ 'Company', false, '2021', false, false, true, true, true,'Singapore', 'Classified', '', '',
+                        "Company, the dedicated website introducing the mobile app, where an intuitively designed platform ensures a smooth and enjoyable exploration of our innovative features.<ol><li>User-Friendly Navigation:<br>Easily navigate the website to discover key information about our mobile app.</li><li>Distinctive Design:<br>Design the buttons with distinctive App Store and Google Play logos to make them recognizable and aligned with the respective platform's branding, enhancing user trust.</li><li>Responsive and Clickable:<br>Ensure that the buttons are responsive and clickable, providing users with a straightforward and intuitive path to download the app on their preferred platform.</li></ol>" ];
+var capitalLink =       [ 'Capital LINK', true, '2021', false, false, true, true, true,'Taiwan', 'Capital Link', 'Facebook', 'https://www.facebook.com/capitallink.tw',
+                        "Capital LINK, the dedicated website for our professional capital team, where a meticulously crafted interface ensures an intuitive and seamless exploration of our financial services.<ol><li>Comprehensive Expertise Showcase:<br>Showcase the diverse expertise of each team member with clear visuals and succinct descriptions.</li><li>Interactive Financial Tools:<br>Explore powerful and interactive financial tools designed to assist our clients in making informed decisions.</li><li>Timely Market Insights:<br>Stay informed with real-time market insights and updates, providing valuable information for strategic decision-making.</li></ol>" ];
+var interiorDesign =    [ 'Interior design', false, '2021', false, false, true, true, true,'Taiwan', 'Deco Master', 'Website', 'https://deco-masters.com.tw',
+                        "Interior design, the digital gateway to transformative home aesthetics, where a thoughtfully crafted online space ensures a visually inspiring and seamlessly navigable exploration of interior design possibilities.<ol><li>Inspiring Design Galleries:<br>Explore a diverse range of interior design projects through visually stunning galleries.</li><li>Intuitive Room Categorization:<br>Effortlessly navigate through different rooms and styles with an intuitive categorization system.</li><li>Interactive Consultation Booking:<br>Easily schedule consultations with expert designers through an interactive and user-friendly booking system.</li></ol>" ];
+var social =            [ 'Social', false, '2020', true, true, false, false, false,'Malaysia', 'Classified', '', '',
+                        "Social, the go-to mobile app for connecting with like-minded individuals, where an intuitively designed platform ensures a seamless and enjoyable experience in making meaningful social connections.<ol><li>Swipe-to-Connect:<br>Swiftly swipe through profiles to express interest and make connections effortlessly.</li><li>Real-Time Chatting:<br>Engage in real-time conversations with your matches for immediate connection.</li><li>Effortless Profile Setup:<br>Easily create and customize your profile for an engaging and authentic representation.</li></ol>" ];
+var wolfpack =            [ 'WOLFPACK', true, '2021', true, true, true, true, true,'Taiwan', 'WOLFPACK', 'Instagram', 'https://www.instagram.com/wolfpack_wild/?hl=en',
+                        "WOLFPACK, the comprehensive mobile app for online courses and forums, where an intuitively designed platform ensures a seamless and engaging educational experience.<ol><li>Progress Tracking:<br>Monitor your learning journey with progress tracking features for each enrolled course.</li><li>Diverse Course Selection:<br>Explore a wide range of online courses covering various topics and subjects.</li><li>Interactive Discussion Forums:<br>Engage with a vibrant community through interactive forums for insightful discussions.</li></ol>" ];
+var health =            [ 'Health', false, '2023', true, true, false, false, false,'Hong Kong', 'Classified', '', '',
+                        "Health, the trusted mobile app for mental well-being, where a thoughtfully designed platform fosters a supportive and accessible environment for individuals seeking mental health resources.<ol><li>Personalized Well-Being Plans:<br>Create and customize well-being plans tailored to individual mental health needs.</li><li>Mindfulness and Meditation:<br>Access guided mindfulness sessions and meditation exercises for relaxation and stress relief.</li><li>Emotional Health Journal:<br>Maintain a digital journal to track emotions, thoughts, and reflections, promoting self-awareness.</li></ol>" ];
+var coupon =            [ 'Coupon', true, '2022', false, false, true, true, true,'Taiwan', 'Classified', 'Web App', 'https://portal.typoint.tw/guest?redirectPath=home',
+                        "Coupon, the go-to web app for collecting coupons and stamps, where a seamlessly designed platform ensures a visually intuitive and efficient experience for building your digital collection.<ol><li>Easy Coupon Collection:<br>Effortlessly collect digital coupons and stamps through a user-friendly interface.</li><li>Smart Sorting and Filtering:<br>Organize your collection with smart sorting and filtering options for quick access.</li><li>Real-Time Coupon Updates:<br>Stay informed with instant notifications about the latest coupons and stamp additions.</li></ol>" ];
+var pay =               [ 'Pay', false, '2023', false, false, true, true, true,'Taiwan', 'Classified', '', '',
+                        "Pay, the all-in-one web app payment hub that seamlessly combines multiple payment methods, points, and coupons into a single, visually intuitive platform for a convenient and streamlined transaction experience.<ol><li>Multi-Method Transactions:<br>Enjoy the flexibility of making payments using various methods, all in one place.</li><li>Smart Points Integration:<br>Easily track, redeem, and manage points accrued from different transactions.</li><li>Efficient Coupon Redemption:<br>Redeem and apply coupons seamlessly during transactions for added discounts.</li></ol>" ];
+var p360 =            [ '360', false, '2021', false, false, false, false, true,'Taiwan', 'Classified', '', '',
+                        "360, the comprehensive web destination for 360° payment reports, where an intuitively designed platform ensures a seamless and visually insightful experience in tracking and analyzing your financial transactions.<ol><li>Interactive Transaction Overview:<br>Access a visually comprehensive overview of your financial transactions through an interactive interface.</li><li>Customizable Reporting Filters:<br>Tailor your reports with customizable filters to gain specific insights into payment data.</li><li>Visual Data Analytics:<br>Utilize visual data representations for a clear understanding of payment trends and patterns.</li></ol>" ];
+var qrTap =             [ 'QR Tap', false, '2023', false, true, true, true, true,'Taiwan', 'Classified', '', '',
+                        "QR Tap, the cutting-edge tablet-based payment portal designed for seamless transactions, featuring QR code scanning or credit card tapping, along with detailed order information, ensuring a visually intuitive and efficient payment experience.<ol><li>Effortless Payment Options:<br>Enable quick and easy transactions through QR code scanning or credit card tapping.</li><li>Order Details Display:<br>View comprehensive order details directly on the tablet for enhanced transparency.</li><li>Real-Time Payment Confirmation:<br>Receive instant confirmation of successful payments, enhancing the overall transaction experience.</li></ol>" ];
+var trendspotter =      [ 'TrendSpotter', false, '2020', false, false, true, true, true,'Singapore', 'Personal Project', 'Figma', 'https://www.figma.com/file/LyK9u7kuRe664yAR8BcFmS/TrendSpotter?type=design&t=kqEO4ypMg2lKh4Ja-6',
+                        "TrendSpotter, the go-to website for streamlined hotel information tracking, where an intuitively designed platform ensures a visually seamless experience for accessing and monitoring hotel trends.<ol><li>Dynamic Trend Visualization:<br>Explore hotel trends through visually appealing and interactive charts for comprehensive insights.</li><li>Real-Time Occupancy Status:<br>Stay updated with real-time data on hotel occupancy rates for informed decision-making.</li><li>Comparative Analysis Tools:<br>Utilize tools for comparative analysis between different hotels, facilitating data-driven decision processes.</li></ol>" ];
+
+// Close popup 
+function popClose(e) {
+    if (e.classList.contains('pop'))
+        window.onclick  = function(event) {
+            if (event.target == e) {
+                e.children[0].style.animationName = "popdown";
+                setTimeout(function(){
+                    e.style.display = "none";
+                    e.children[0].style.animationName = "popup";
+                }, 250); }}
+    else if (!(e.classList.contains('popBody'))) {
+        e.closest('.pop').children[0].style.animationName = "popdown";
+        setTimeout(function(){
+            e.closest('.pop').style.display = "none";
+            e.closest('.pop').children[0].style.animationName = "popup";
+        }, 250); }
+    document.body.style.overflow = "scroll"; }
+
+// Show portfolio popup
+function popPortfolio(e) {
+    document.getElementById("popImage").src                 = './img/portfolio/' + eval(e)[0] + '.png';
+    if (eval(e)[1]) {
+        document.getElementById("popLogo").src              = './img/logo/' + eval(e)[0] + '.svg';
+        document.getElementById("popLogo").style.display    = "flex"; }
+    else
+        document.getElementById("popLogo").style.display    = "none";
+    document.getElementById("popTitle").innerHTML           = eval(e)[0];
+    document.getElementById("popDate").innerHTML            = eval(e)[2];
+    if (eval(e)[3])
+        document.getElementById("popIos").classList.remove('disabled');
+    else
+        document.getElementById("popIos").classList.add('disabled');
+    if (eval(e)[4])
+        document.getElementById("popAndroid").classList.remove('disabled');
+    else
+        document.getElementById("popAndroid").classList.add('disabled');
+    if (eval(e)[5])
+        document.getElementById("popMobile").classList.remove('disabled');
+    else
+        document.getElementById("popMobile").classList.add('disabled');
+    if (eval(e)[6])
+        document.getElementById("popTablet").classList.remove('disabled');
+    else
+        document.getElementById("popTablet").classList.add('disabled');
+    if (eval(e)[7])
+        document.getElementById("popDesktop").classList.remove('disabled');
+    else
+        document.getElementById("popDesktop").classList.add('disabled');
+    document.getElementById("popLocation").innerHTML      = eval(e)[8];
+    if (eval(e)[9]) {
+        document.getElementById("popClient").innerHTML      = eval(e)[9];
+        document.getElementById("popTable").getElementsByTagName('tr')[3].style.display = "table-row"; }
+    else
+        document.getElementById("popTable").getElementsByTagName('tr')[3].style.display = "none";
+    if (eval(e)[10]) {
+        document.getElementById("popLink").innerHTML        = eval(e)[10];
+        document.getElementById("popLink").href             = eval(e)[11];
+        document.getElementById("popTable").getElementsByTagName('tr')[4].style.display = "table-row"; }
+    else
+        document.getElementById("popTable").getElementsByTagName('tr')[4].style.display = "none";
+    document.getElementById("popDescription").innerHTML     = eval(e)[12];
+    document.getElementById("popPortfolio").style.display   = "flex";
+    document.getElementById("popDetail").scrollTop = 0;
+    document.body.style.overflow = "hidden"; }
+
+// Textarea
+function textarea(element) {
+    element.style.height = "5px";
+    element.style.height = (element.scrollHeight) + "px"; }
